5e75f48a8a758a7650c68b5f3d1a206f0086f93a
antheboets <anthe.boets@gmail.com>
2019-08-12 00:33:46 +0200

    adding contact

--- Logic/mail.php
+++ Logic/mail.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
+    include_once($_SERVER['DOCUMENT_ROOT']."/anthe.boets/public_html/eShop/Logic/lib.php");
 	if($_SERVER["REQUEST_METHOD"] == 'POST'){
 		if(isset($_POST['email']) && isset($_POST['header']) && isset($_POST['body']) && isset($_POST['send'])){
 			if(!empty($_POST['email']) && !empty($_POST['header']) && !empty($_POST['body']) && !empty($_POST['send'])){
 				$headers[] = 'From: <'.$_POST['email'].'>';
 				mail('anthe.boets@student.ehb.be',$_POST['header'],$_POST['body'],implode("\r\n", $headers));
-				header("location : ".$_SERVER['DOCUMENT_ROOT']."/anthe.boets/public_html/TacGen/TacGen/UI/pages/contact.php");
+				header("location : ".$path."UI/pages/contact.php");
 			}
 		}
 	}
