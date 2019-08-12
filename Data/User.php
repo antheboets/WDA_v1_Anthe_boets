2adc57d8da4772f69dd65cbfd3b544eea954d035
antheboets <anthe.boets@gmail.com>
2019-08-12 09:00:17 +0200

    Update User.php

--- Data/User.php
+++ Data/User.php
@@ -8,14 +8,16 @@
         public $shoppingCart;
         public $admin;
 
-        public function __construct($id, $email, $firstName, $lastName, $shoppingCart)
+        public function __construct($id, $email, $firstName, $lastName, $shoppingCart, $admin)
         {
             $this->id = $id;
             $this->email = $email;
             $this->firstName = $firstName;
             $this->lastName = $lastName;
             $this->shoppingCart = $shoppingCart;
+            $this->admin = $admin;
         }
+
 
     }
 
