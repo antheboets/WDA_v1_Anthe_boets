8257c989fe73ea5cd67d26c96b63f037465b8b2d
antheboets <anthe.boets@gmail.com>
2019-08-12 22:24:18 +0200

    adding isFloat and isInt functions

--- UI/js/lib.js
+++ UI/js/lib.js
@@ -2,4 +2,13 @@
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+
+}
+//https://stackoverflow.com/questions/3885817/how-do-i-check-that-a-number-is-float-or-integer
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
 }
