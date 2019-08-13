ba7df6d51e27098c7cfdf5b5b13336f311ab0beb
antheboets <anthe.boets@gmail.com>
2019-08-14 00:07:01 +0200

    fixing comment family hierarchy logic

--- Database/CommentDAO.php
+++ Database/CommentDAO.php
@@ -4,7 +4,7 @@ include_once($path."Data/Comment.php");
 include_once($path."Data/Rating.php");
 class CommentDAO{
     public static function getAllCommentsOfProduct($ProductId){
-        $rows = DatabaseFactory::getDatabase()->executeQuery( "SELECT C.*, U.FirstName, U.LastName , R.Rating FROM Comment AS C INNER JOIN User AS U ON(C.UserId=U.UserId) INNER JOIN Rating AS R ON(C.ProductId=R.ProductId AND C.UserId=R.UserId) WHERE C.ProductId = '?' ORDER BY CreationTime ASC",array($ProductId));
+        $rows = DatabaseFactory::getDatabase()->executeQuery( "SELECT C.*, U.FirstName, U.LastName , R.Rating FROM Comment AS C INNER JOIN User AS U ON(C.UserId=U.UserId) INNER JOIN Rating AS R ON(C.ProductId=R.ProductId AND C.UserId=R.UserId) WHERE C.ProductId = '?' ORDER BY CreationTime ASC;",array($ProductId));
         return self::convertRowToObject($rows);
     }
 
@@ -14,7 +14,7 @@ class CommentDAO{
             $foundReplyHost = false;
             foreach ($rows as $row){
                 if(!is_null($row['ReplyId'])) {
-                    for($i = 0; $i < sizeof($comments); $i++ ){
+                    /*for($i = 0; $i < sizeof($comments); $i++ ){
                         if($comments[$i]->id == $row['ReplyId']){
                             $foundReplyHost = true;
                             $replyComment = new Comment($row['CommentId'],$row['Text'],($row['FirstName'] ." ".$row['LastName']),$row['Rating'],[],$row['CreationTime']);
@@ -24,7 +24,10 @@ class CommentDAO{
                     }
                     if(!$foundReplyHost){
                         $comments[] = new Comment($row['CommentId'],$row['Text'],($row['FirstName'] ." ".$row['LastName']),$row['Rating'],[],$row['CreationTime']);
-                    }
+                    }*/
+                    $replyComment = new Comment($row['CommentId'],$row['Text'],($row['FirstName'] ." ".$row['LastName']),$row['Rating'],[],$row['CreationTime']);
+                    $returnVal = self::putReplyInComment($comments,$replyComment,$row['ReplyId']);
+                    $comments = $returnVal[1];
                 }
                 else{
                     $comments[] = new Comment($row['CommentId'],$row['Text'],($row['FirstName'] ." ".$row['LastName']),$row['Rating'],[],$row['CreationTime']);
@@ -33,6 +36,36 @@ class CommentDAO{
             return $comments;
         }
         return NULL;
+    }
+    private static function putReplyInComment($comments,$commentNew,$id){
+        $found = false;
+        /*
+        for($i = 0; $i < sizeof($comments); $i++ ) {
+            if($comments[$i]->id == $id){
+                $found = true;
+                $comments[]->replys[] = $commentNew;
+                return array($found,$comments);
+                break;
+            }
+        }
+        */
+        foreach ($comments as $comment){
+            if($comment->id == $id){
+                $found = true;
+                $comment->replys[] = $commentNew;
+                return array($found,$comments);
+                break;
+            }
+        }
+        foreach ($comments as $comment){
+            $returnVal = self::putReplyInComment($comment->replys,$commentNew,$id);
+            if($returnVal[0]){
+                //$comment->replys[] = $commentNew;
+                $found = true;
+                return(array($found,$comments));
+            }
+        }
+        return array($found,$comments);
     }
 }
 ?>
