40dc6c17ec9c23d8fbfa73aa5dcacf04174ed836
antheboets <anthe.boets@gmail.com>
2019-08-14 05:37:36 +0200

    Update header.php

--- UI/component/header.php
+++ UI/component/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	<a class="navbar-brand" href="#">eShop</a>
+	<a class="navbar-brand" href="<?php echo $url ;?>index.php">eShop</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		</button>
@@ -12,7 +12,7 @@
                 <a class="nav-link" href="#">Products</a>
             </li>
   			<li class="nav-item">
-    			<a class="nav-link" href="http://dtsl.ehb.be/~anthe.boets/eShop/UI/pages/contact.php">Contact</a>
+    			<a class="nav-link" href="<?php echo $url ;?>UI/pages/contact.php">Contact</a>
   			</li>
 		</ul>
         <ul class="navbar-nav ml-auto">
