e35bbda46633abb5a9087e4b07740010ae5b78da
antheboets <anthe.boets@gmail.com>
2019-08-14 05:33:28 +0200

    adding drawComment

--- Data/Comment.php
+++ Data/Comment.php
@@ -20,5 +20,34 @@ include_once($path."Data/Rating.php");
         }
 
 
+        public function drawComment($level){
+            $level++;
+            if($level % 2 == 0 ){
+                $classTheme = "commentLight";
+            }
+            else{
+                $classTheme = "commentDark";
+            }
+            echo "<div class='comment ".$classTheme."' id='".$this->id."'>";
+                echo "<div class='row'>";
+                    echo "<div class='col-md-2'>";
+                        echo "<p class='noBreak commentInfo'>Name: ".$this->user."</p>";
+                        echo "<p class='noBreak commentInfo'>Rating: ".$this->rating."/10</p>";
+                        echo "<p class='noBreak commentInfo'>Time: ".$this->creationTime."</p>";
+                        echo "<button class='commentInfo'><a href='".$this->getUrl()."'>reply</a></button>";
+                    echo "</div>";
+                    echo "<div class='commentText col-md-10'>";
+                        echo "<p class='noBreak'>".$this->text."</p>";
+                    echo "</div>";
+                echo "</div>";
+                echo "<div class='replys'>";
+                    if(!empty($this->replys)){
+                        foreach ($this->replys as $reply){
+                            $reply->drawComment($level);
+                        }
+                    }
+                echo "</div>";
+            echo "</div>";
+        }
     }
 ?>
