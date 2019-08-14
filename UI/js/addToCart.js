65bcfe04646c78ef36179e775a8a492595b3fa76
antheboets <anthe.boets@gmail.com>
2019-08-14 08:45:42 +0200

    Update addToCart.js

--- UI/js/addToCart.js
+++ UI/js/addToCart.js
@@ -18,31 +18,46 @@ $(document).ready(function() {
         for (var i = 0; i < classname.length; i++) {
             classname[i].addEventListener('click', e => {
 
-
-                let data = {
-                    "productId" : e.currentTarget.parentElement.id,
-                    "amount" : "1"
-                };
-
-
-                $.ajax({
-                    async: true,
-                    crossDomain: true,
-                    method: "POST",
-                    url:  url+ "Logic/addProductToCart.php",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    data: JSON.stringify(data),
-                    dataType: 'json',
-
-                    success: function(data){
-                        console.log(data);
-                    },
-                    error: function(data){
-                        console.log(data)
+                let error = false;
+                if(document.getElementById('buyAmount') != null){
+                    if(!isInt(document.getElementById('buyAmount').value)){
+                        error = true;
+                        document.getElementById('IBuyAmount').innerText = "must be a number";
                     }
-                });
+
+
+                    if(!error){
+
+                        let amount = 1;
+
+                        let data = {
+                            "productId" : e.currentTarget.parentElement.id,
+                            "amount" : amount
+                        };
+
+
+                        $.ajax({
+                            async: true,
+                            crossDomain: true,
+                            method: "POST",
+                            url:  url+ "Logic/addProductToCart.php",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            data: JSON.stringify(data),
+                            dataType: 'json',
+
+                            success: function(data){
+                                console.log(data);
+                            },
+                            error: function(data){
+                                console.log(data)
+                            }
+                        });
+
+                    }
+                }
+
 
 
 
